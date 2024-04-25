71fda546ef1032f6095dcf63c6518bcb27d97abb
ramiro alet <ramiro.alet@gmail.com>
2024-04-25 16:01:37 -0300

    database

--- server/src/database.js
+++ server/src/database.js
@@ -3,7 +3,7 @@ import { DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD } from './config.js';
 
 const sequelize = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: 'mysql', // Cambia el dialecto a 'mysql'
+  dialect: 'postgres',
   logging: false,
 });
 
--- server/src/index.js
+++ server/src/index.js
@@ -5,8 +5,8 @@ sequelize.sync({ force: false }).then(() => {
     console.log('All models were synchronized successfully.');
   
     // Inicia el servidor después de que los modelos se hayan sincronizado
-    server.listen(3000, () => {
-      console.log(`Server on port 3000`);
+    server.listen(80, () => {
+      console.log(`Server on port 80`);
     });
   }).catch(error => {
     console.error('Unable to synchronize the models:', error);
