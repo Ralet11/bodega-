238a03cdd62123499df94333bff15fecb5c464c9
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-05-09 16:47:08 -0300

    Mejoras en navegacion de productos distribuidores.

--- client/src/ui_bodega/ButtonBodega.jsx
+++ client/src/ui_bodega/ButtonBodega.jsx
@@ -4,7 +4,7 @@ const ButtonBodega = ({ children, className,  ...rest }) => {
   return (
     <button
       {...rest}
-      className={`mt-5 px-6 font-bold rounded-full  text-yellow-600 bg-black border border-white hover:text-black hover:bg-yellow-600 ${className}`}
+      className={` font-bold rounded-full border border-white ${className}`}
        // Aquí establecemos el ancho usando la prop 'width'
     >
       {children}
