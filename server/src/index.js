950dc374fbbe370b76a7740ca217e1289e2916d4
Apptrader <proyectoapptrader@gmail.com>
2024-05-15 14:18:39 -0300

    new server

--- server/src/index.js
+++ server/src/index.js
@@ -11,3 +11,31 @@ sequelize.sync({ force: false }).then(() => {
   }).catch(error => {
     console.error('Unable to synchronize the models:', error);
   });
+
+  /*
+  import app from "./server.js";
+import sequelize from "./database.js";
+import fs from 'fs';
+import https from 'https'
+
+
+
+sequelize.sync({ force: false }).then(() => {
+  const privateKey = fs.readFileSync('./../certificados/private.key', 'utf8');
+  const certificate = fs.readFileSync('./../certificados/certificate.crt', 'utf8');
+  const ca = fs.readFileSync('./../certificados/ca_bundle.crt', 'utf8');
+  
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+  
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(443, () => {
+    console.log('Servidor HTTPS está escuchando en el puerto 443');
+  }); 
+   
+  }).catch(error => {
+    console.error('Unable to synchronize the models:', error);
+  }); */
