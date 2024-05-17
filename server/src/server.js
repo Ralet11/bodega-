e177df9b226165e93315ddffa1bd4b777bce41ef
Apptrader <proyectoapptrader@gmail.com>
2024-05-17 19:53:08 -0300

    cambios ramiro

--- server/src/server.js
+++ server/src/server.js
@@ -92,19 +92,31 @@ import discountRouter from './routes/discounts.routes.js';
 import DistProductRouter from './routes/distProducts.routes.js';
 import distOrderRouter from './routes/distOrders.routes.js';
 import distOrderStatusRouter from "./routes/distOrderStatus.routes.js";
-import { FRONTEND_URL } from "./config.js";
+import { FRONTEND_URL, SSK } from "./config.js";
+import Stripe from "stripe";
+import cookieParser from 'cookie-parser';
+import bodyParser from "body-parser";
 
 const app = express();
+const stripe = new Stripe(SSK);
 
+const endpointSecret = "whsec_9d9dffedc83b18c6cb3f360a0332c541e2f9a2362d625d1968196a540566d3d6";
+
+// Middleware
 app.use(morgan("dev"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb" }));
+app.use(bodyParser.raw({ type: "*/*" }))
+app.use(cookieParser());
 
+// Configuración de CORS
 const corsOptions = {
   origin: [`${FRONTEND_URL}`, "exp://192.168.75.227:8081"],
 };
 app.use(cors(corsOptions));
-app.use('/uploads', express.static('uploads'))
+
+// Rutas
+app.use('/uploads', express.static('uploads'));
 app.use("/api/auth", authRoutes);
 app.use("/api/local", localRoutes);
 app.use("/api/categories", categoryRoutes);
@@ -118,9 +130,99 @@ app.use("/api/addresses", addresesRouter);
 app.use("/api/discounts", discountRouter);
 app.use("/api/distProducts", DistProductRouter);
 app.use("/api/distOrder", distOrderRouter);
-app.use("/api/distOrderStatus", distOrderStatusRouter)
+app.use("/api/distOrderStatus", distOrderStatusRouter);
 
-export default app; 
+// Ruta del webhook de Stripe
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  async (request, response) => { 
+    const sig = request.headers["stripe-signature"];
+    const bodyString = request.body.toString();
+    let event;
 
+    try {
+      event = stripe.webhooks.constructEvent(
+        bodyString,
+        sig,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`Webhook Error: ${err.message}`);
+      return response.status(400).send(`Webhook Error: ${err.message}`);
+    }
 
+    let resData = "";
 
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntent = event.data.object;
+        customer = paymentIntent.customer
+        payMethod = paymentIntent.payment_method
+        if (paymentIntent.amount === 60000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P2MQQCtqRjqS5chcgCR4WJ2", payMethod)
+        } else if(paymentIntent.amount === 25000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P2MPqCtqRjqS5chqiRgs0jA", payMethod)
+        } else if (paymentIntent.amount === 15000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P2LOSCtqRjqS5ch03lA7nKo", payMethod)
+        }
+
+        /* if (paymentIntent.amount === 60000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P4XNOIrqUJwwaEOzJfiunHk", payMethod)
+        } else if(paymentIntent.amount === 25000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P4XN7IrqUJwwaEO74KcP17W", payMethod)
+        } else if (paymentIntent.amount === 15000) {
+          await crearSuscripcionConMetodoDePago(customer, "price_1P4XLgIrqUJwwaEOjHD2sGNq", payMethod)
+        } */
+
+      case "charge.succeeded":
+         payMethod = event.data.object.payment_method;
+         stripe.paymentIntents.retrieve(
+          event.data.object.id,
+          function(err, paymentIntent) {
+              if (err) {
+                  console.error("Error al recuperar la información del pago:", err);
+                  // Maneja el error de alguna manera adecuada
+              } else {
+                  console.log("Información del pago:", paymentIntent);
+                  
+              }
+          }
+      );
+         /* stripe.customers.retrieve(
+          event.data.object.customer,
+          function(err, customer) {
+              if (err) {
+                  console.error("Error al recuperar la información del cliente:", err);
+                  // Maneja el error de alguna manera adecuada
+              } else {
+                  console.log("Información del cliente:", customer);
+                  // Aquí puedes acceder a toda la información del cliente
+                  resData = customer;
+              }
+          }
+      ); */
+        break;
+      case "customer.created":
+        customer = event.data.object
+        break
+      case "customer.subscription.created":
+        const subscription = event.data.object;
+        subId = subscription.id; 
+        subPlanId = subscription.plan.id
+
+        break;
+      case 'checkout.session.completed':
+       /*  resData = event.data.object */
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+;
+
+ /*    return response.status(200).send(resData); */
+  }
+);
+
+export default app;
