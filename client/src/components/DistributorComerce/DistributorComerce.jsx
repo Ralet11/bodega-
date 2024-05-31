1237ff04e421bedb6b5afc23d140d3cda178576a
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-05-30 21:21:28 -0300

    feat: market mobile good

--- client/src/components/DistributorComerce/DistributorComerce.jsx
+++ client/src/components/DistributorComerce/DistributorComerce.jsx
@@ -118,61 +118,61 @@ const DistributorComerce = () => {
       <ProductSlider />
       <div className="flex flex-col px-2 justify-center w-full md:w-[80%] mt-8">
         <h2 className="text-base font-bold text-gray-800 mb-4">Find your products</h2>
-        <div className="mt-8 flex flex-col">
+        <div className="mt-8 flex flex-col justify-center items-center">
           {filteredProducts.length === 0 ? (
             <div className="flex justify-center items-center h-full">
               <h1 className="text-2xl text-center font-bold text-gray-700">Products not found</h1>
             </div>
           ) : (
             filteredProducts.map((product, index) => (
-  <div
-    key={index}
-    className={`flex flex-col md:flex-row border h-auto bg-white rounded-lg overflow-hidden shadow-md transition-transform transform hover:scale-105 hover:shadow-lg ${hoveredIndex === index ? 'shadow-lg' : ''}`}
-    onMouseEnter={() => handleHover(index)}
-    onMouseLeave={() => handleHover(-1)}
-    onClick={() => goToDetail(product)}
-  >
-    <div className="relative w-full md:w-1/3 overflow-hidden">
-      <img
-        className="w-full h-32 md:h-full object-cover transition-transform duration-500 transform hover:scale-110"
-        src={hoveredIndex === index ? product.image2 : product.image1}
-        alt={product.name}
-      />
-    </div>
-    <div className="p-4 flex flex-col justify-between w-full">
-      <h3 className="font-semibold text-sm md:text-xl text-gray-800 mb-2">{product.name}</h3>
-      <p className="text-gray-700 text-base md:text-lg font-bold mb-2">${product.price.toFixed(2)}</p>
-      <p className="text-xs md:text-sm text-red-500 mb-2">
-        <span className="line-through">${(product.price + 50).toFixed(2)}</span> 10% OFF
-      </p>
-      <p className="text-xs md:text-sm text-gray-500 mb-4">{product.description}</p>
-      <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-4">
-        <p className="text-xs text-gray-500">Tienda del Producto</p>
-        {product.freeShipping && (
-          <p className="text-green-500 text-xs md:text-sm ml-0 md:ml-2">Envío gratis</p>
-        )}
-      </div>
-      <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
-        <button className="py-1 md:py-2 px-2 md:px-4 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300 mb-2 md:mb-0">
-          Add to Cart
-        </button>
-        <p className="text-xs md:text-sm text-gray-500 mb-2 md:mb-0">Availability: <span className="text-green-500">In Stock</span></p>
-        <button className="text-gray-500 hover:text-red-500 transition-colors duration-300">
-          <HeartIcon className="w-4 md:w-6 h-4 md:h-6" />
-        </button>
-      </div>
-      <div className="flex items-center mt-4">
-        <span className="text-yellow-500 flex items-center">
-          {Array.from({ length: product.rating }, (_, i) => (
-            <svg key={i} className="w-3 md:w-4 h-3 md:h-4 fill-current" viewBox="0 0 24 24">
-              <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
-            </svg>
-          ))}
-        </span>
-      </div>
-    </div>
-  </div>
-))
+                <div
+                  key={index}
+                  className={`flex flex-col md:flex-row justify-center items-center   w-[360px] md:w-full mb-2 border h-auto bg-white rounded-lg overflow-hidden shadow-md transition-transform transform hover:scale-105 hover:shadow-lg ${hoveredIndex === index ? 'shadow-lg' : ''}`}
+                  onMouseEnter={() => handleHover(index)}
+                  onMouseLeave={() => handleHover(-1)}
+                  onClick={() => goToDetail(product)}
+                >
+                  <div className="relative w-full md:w-1/3 overflow-hidden">
+                    <img
+                      className="w-full h-32 md:h-full object-cover transition-transform duration-500 transform hover:scale-110"
+                      src={hoveredIndex === index ? product.image2 : product.image1}
+                      alt={product.name}
+                    />
+                  </div>
+                  <div className="p-4 flex flex-col justify-between w-full">
+                    <h3 className="font-semibold text-sm md:text-xl text-gray-800 mb-2">{product.name}</h3>
+                    <p className="text-gray-700 text-base md:text-lg font-bold mb-2">${product.price.toFixed(2)}</p>
+                    <p className="text-xs md:text-sm text-red-500 mb-2">
+                      <span className="line-through">${(product.price + 50).toFixed(2)}</span> 10% OFF
+                    </p>
+                    <p className="text-xs md:text-sm text-gray-500 mb-4">{product.description}</p>
+                    <div className="flex flex-col md:flex-row items-start md:items-center justify-between mb-4">
+                      <p className="text-xs text-gray-500">Tienda del Producto</p>
+                      {product.freeShipping && (
+                        <p className="text-green-500 text-xs md:text-sm ml-0 md:ml-2">Envío gratis</p>
+                      )}
+                    </div>
+                    <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
+                      <button className="py-1 md:py-2 px-2 md:px-4 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition duration-300 mb-2 md:mb-0">
+                        Add to Cart
+                      </button>
+                      <p className="text-xs md:text-sm text-gray-500 mb-2 md:mb-0">Availability: <span className="text-green-500">In Stock</span></p>
+                      <button className="text-gray-500 hover:text-red-500 transition-colors duration-300">
+                        <HeartIcon className="w-4 md:w-6 h-4 md:h-6" />
+                      </button>
+                    </div>
+                    <div className="flex items-center mt-4">
+                      <span className="text-yellow-500 flex items-center">
+                        {Array.from({ length: product.rating }, (_, i) => (
+                          <svg key={i} className="w-3 md:w-4 h-3 md:h-4 fill-current" viewBox="0 0 24 24">
+                            <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                          </svg>
+                        ))}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))
           )}
         </div>
       </div>
