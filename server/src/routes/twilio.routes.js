9167e3e580551553f5498e43c9bb3db5da4bafd4
Apptrader <proyectoapptrader@gmail.com>
2024-10-14 18:38:02 -0300

    commit

--- server/src/routes/twilio.routes.js
+++ server/src/routes/twilio.routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { makeCall, voiceResponse, handleKeyPress } from '../controller/twilio.controller.js';
+/* import { makeCall, voiceResponse, handleKeyPress } from '../controller/twilio.controller.js';
 
 const router = express.Router();
 
 router.post('/make-call', makeCall);
 router.post('/voice', voiceResponse);
-router.post('/handle-key', handleKeyPress);
+router.post('/handle-key', handleKeyPress); */
 
 export default router;
